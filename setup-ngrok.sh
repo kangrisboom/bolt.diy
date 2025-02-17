1383f33f6a9ee9f0d1b1df7b4bd22282a0c2e4a1
kangrisboom <kangrisboom@gmail.com>
2025-02-17 16:05:53 +0700

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -4,6 +4,3 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/sources.list.d/ngrok.list \
   && sudo apt update \
   && sudo apt install ngrok
-  && ngrok config add-authtoken 2tA1ZmFvVMXhd0UXoRCZBdomNbe_6av3o94w32peSiCy6jJjr
-  && ngrok http 5173
-
